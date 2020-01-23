2f1654df148b87e4f5faf498591ed2405efccc05
bethebikash <bhattaraibikash55@gmail.com>
2020-01-23 18:52:47 +0545

    react setup and tested by creating component.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,10 @@
-console.log('test')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Template = () => {
+  return <div>
+    <h1>Hello from react component</h1>
+  </div>
+}
+
+ReactDOM.render(<Template/> , document.getElementById('app'))
