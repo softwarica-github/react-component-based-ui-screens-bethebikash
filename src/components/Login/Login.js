d4a2c647eb06a3ab96aa7813705bc166add33b29
bethebikash <bhattaraibikash55@gmail.com>
2020-01-23 23:46:24 +0545

    login form added.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import {Link } from 'react-router-dom'
+
+import {Form, Button } from 'react-bootstrap'
 
 export default class Login extends Component {
   render() {
     return (
-      <div>
-        This is from Login Component
+      <div className="my-container">
+        <Form>
+          <Form.Group controlId="formBasicUsername">
+            <Form.Label>Username address</Form.Label>
+            <Form.Control type="Username" placeholder="Enter Username" />
+            <Form.Text className="text-muted">
+              We'll never share your Username with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <p>Dont have an account? <Link to="/register">Register</Link></p>
+          <Button as={Link} to="/" variant="primary" type="submit" className="my-btn">
+            Submit
+          </Button>
+        </Form>
       </div>
     )
   }
