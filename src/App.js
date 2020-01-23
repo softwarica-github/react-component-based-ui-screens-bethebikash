9ce59f8a4bb0afda992a9c0a92b3aaf93e55dd10
bethebikash <bhattaraibikash55@gmail.com>
2020-01-23 19:15:38 +0545

    React router setup and used to render different components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom'
 
-const Template = () => {
-  return <div>
-    <h1>Hello from react component</h1>
-  </div>
+const Dashboard = () => {
+  return(
+    <div>This is from Dashboard Component</div>
+  )
 }
 
-ReactDOM.render(<Template/> , document.getElementById('app'))
+const Login = () => {
+  return(
+    <div>This is from Login Component</div>
+  )
+}
+
+const Register = () => {
+  return(
+    <div>This is from Register Component</div>
+  )
+}
+
+const About = () => {
+  return(
+    <div>This is from About Component</div>
+  )
+}
+
+const Template = () => {
+  return (
+    <Router>
+      <div>
+        <header>
+          <h1>eRepair</h1>
+          <NavLink to="/" exact>
+            Dashboard
+          </NavLink>
+          <NavLink to="/about">
+            About
+          </NavLink>
+          <NavLink to="/login">
+            Login
+          </NavLink>
+        </header>
+        <Switch>
+          <Route path="/" component={Dashboard} exact />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+ReactDOM.render(<Template />, document.getElementById('app'))
