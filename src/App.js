bc143541b93931ee0a9de961414a2046daf4b550
bethebikash <bhattaraibikash55@gmail.com>
2020-01-23 22:15:40 +0545

    Nav bar created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   BrowserRouter as Router,
   Route,
   NavLink,
+  Link,
   Switch
 } from 'react-router-dom'
 import './style.css'
@@ -11,24 +12,43 @@ import Dashboard from './components/Dashboard/Dashboard'
 import Login from './components/Login/Login'
 import Register from './components/Register/Register'
 import About from './components/About/About'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { Navbar, Nav } from 'react-bootstrap'
 
 const Template = () => {
   return (
     <Router>
       <div>
         <header>
-          <h1>eRepair</h1>
-          <NavLink to="/" exact>
-            Dashboard
-          </NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/login">Login</NavLink>
+          <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <div className="container">
+                <Nav className="mr-auto">
+                  <Nav.Link exact as={NavLink} activeClass="active" to="/">
+                    Dashboard
+                  </Nav.Link>
+                  <Nav.Link as={NavLink} to="/about">
+                    About
+                  </Nav.Link>
+                </Nav>
+                <Nav>
+                  <Nav.Link as={NavLink} to="/login">
+                    Login
+                  </Nav.Link>
+                </Nav>
+              </div>
+            </Navbar.Collapse>
+          </Navbar>
         </header>
         <Switch>
-          <Route path="/" component={Dashboard} exact />
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-          <Route path="/about" component={About} />
+          <div className="container">
+            <Route path="/" component={Dashboard} exact />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+            <Route path="/about" component={About} />
+          </div>
         </Switch>
       </div>
     </Router>
