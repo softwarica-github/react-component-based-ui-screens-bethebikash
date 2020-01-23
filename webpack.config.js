7b7b96abd915fb51ba45d0f61cfd4d762896309e
bethebikash <bhattaraibikash55@gmail.com>
2020-01-23 18:40:43 +0545

    @babel and webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/App.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    devServer:{
+        contentBase: path.resolve(__dirname, 'public'),
+        port:3000,
+        hot:true,
+        publicPath: 'http://localhost:3000/dist/'
+    },
+    devtool:'inline-source-map'
+}
